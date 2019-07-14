9c0dc885a202f96d2271325cd91ddd9a1b0f027a
Fallentol <lugvas1@gmail.com>
2019-07-14 15:53:41 +0300

    Commit 1.5

--- src/Config.java
+++ src/Config.java
@@ -2,14 +2,16 @@ public class Config {
 
     public static final int startX = 1;
     public static final int startY = 1;
-    public static final int finishX = 18;
-    public static final int finishY = 19;
+    public static final int finishX = 28;
+    public static final int finishY = 29;
 
-    public static final int HEIGHT = 20;
-    public static final int WIDTH = 20;
+    public static final int HEIGHT = 30;
+    public static final int WIDTH = 30;
     public static final int LAG = 2000;
     public static final int obstacleSign = 99;
     public static final int startSign = 1;
     public static final int finishSign = 88;
-    public static final int randomObstacle = 40;
+    public static final int randomObstacle = 75;
+    public static final int randomObstacleLength = 5;
+    public static final int randomObstacleNumber = 10;
 }
--- src/Obstacle.java
+++ src/Obstacle.java
@@ -20,6 +20,12 @@ public class Obstacle {
         setObstacle(5, 13);
         setObstacle(5, 14);
         setObstacle(5, 15);
+
+
+        setObstacle(14, 9);
+        setObstacle(13, 9);
+        setObstacle(12, 9);
+        setObstacle(15, 9);
         setObstacle(14, 10);
         setObstacle(14, 11);
         setObstacle(14, 12);
@@ -28,13 +34,24 @@ public class Obstacle {
         setObstacle(14, 15);
         setObstacle(14, 16);
         setObstacle(14, 17);
+        setObstacle(14, 18);
+        setObstacle(14, 19);
+
+
         setObstacle(15, 12);
+
         setObstacle(0, 7);
         setObstacle(1, 7);
         setObstacle(2, 7);
-        setObstacle(4, 7);
         setObstacle(5, 7);
+        setObstacle(5, 8);
 
+        setObstacle(10, 1);
+        setObstacle(10, 2);
+        setObstacle(10, 3);
+        setObstacle(10, 4);
+        setObstacle(10, 5);
+        setObstacle(10, 6);
         setObstacle(10, 7);
         setObstacle(10, 8);
         setObstacle(10, 9);
@@ -43,8 +60,29 @@ public class Obstacle {
         setObstacle(10, 12);
         setObstacle(10, 13);
         setObstacle(10, 14);
-        setObstacle(10, 15);
+        setObstacle(11, 14);
+        setObstacle(12, 14);
+
+
+        setObstacle(16, 24);
+        setObstacle(16, 23);
+        setObstacle(16, 25);
+        setObstacle(15, 25);
+        setObstacle(14, 25);
+        setObstacle(13, 25);
+        setObstacle(12, 25);
+        setObstacle(11, 25);
+        setObstacle(10, 25);
+        setObstacle(9, 25);
+        setObstacle(8, 25);
+        setObstacle(7, 25);
+        setObstacle(6, 25);
+        setObstacle(5, 25);
+        setObstacle(4, 25);
+        setObstacle(1, 25);
+
         getRandomObstacle();
+        getRandomStringObstacle();
 
 
         return OBSTACLE;
@@ -53,6 +91,28 @@ public class Obstacle {
     public static void getRandomObstacle() {
         for (int i = 0; i < Config.randomObstacle; i++)
             setObstacle(new Random().nextInt(Config.WIDTH), new Random().nextInt(Config.HEIGHT));
+    }
+
+    public static void getRandomStringObstacle() {
+        for (int m = 0; m < Config.randomObstacleNumber; m++) {
+            int randomX = new Random().nextInt(Config.WIDTH);
+            int randomY = new Random().nextInt(Config.WIDTH);
+            int direction = new Random().nextInt(3);
+            setObstacle(randomX, randomY);
+
+            for (int i = 0; i < Config.randomObstacleLength; i++) {
+                if (direction == 0) { // up
+                    if (randomY + 1 < Config.HEIGHT) setObstacle(randomX, ++randomY);
+                } else if (direction == 1) {
+                    if (randomY - 1 > 0) setObstacle(randomX, --randomY);
+                } else if (direction == 2) {
+                    if (randomX + 1 < Config.WIDTH) setObstacle(++randomX, randomY);
+                } else {
+                    if (randomX - 1 > 0) setObstacle(--randomX, randomY);
+                }
+            }
+        }
+
     }
 
     public static void setObstacle(int x, int y) {
